70d18c0792938955012a188fa17c286ae9450132
Henric Hemmander <henric1@hotmail.se>
2021-12-21 20:51:17 +0100

    fixed some colors in left sidebar

--- backend-whatsapp/routes/message.routes.js
+++ backend-whatsapp/routes/message.routes.js
@@ -1,5 +1,7 @@
 import MessageController from '../controllers/message.controller.js'
 
+// const MessageController = require('../controllers/message.controller.js')
+
 const routes = (app) => {
   app.get('/', MessageController.defaultPath)
   app.get('/messages/sync', MessageController.getAllMessages)
